26ef256ca4b9fd20eea86b0c5141821940ced9dc
thomazf <thomaz.fneves@gmail.com>
2023-01-13 22:22:01 -0300

    fim traducao + ajuste limite de caracteres em DTO

--- src/main/java/br/com/possoler/api/api_posso_ler/api/core_api/dto/DadosAcessoDTO.java
+++ src/main/java/br/com/possoler/api/api_posso_ler/api/core_api/dto/DadosAcessoDTO.java
@@ -5,19 +5,17 @@ import java.time.LocalTime;
 
 import br.com.possoler.api.api_posso_ler.api.core_api.model.DadosAcesso;
 import br.com.possoler.api.api_posso_ler.api.core_api.model.Site;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Builder
 @Data
 public class DadosAcessoDTO {
     
     @NotNull(message = "O campo 'codigo_site' não pode ser vazio")
-    @Min(value = 1, message = "Jornal invalido")
-    @Max(value = 38, message = "Jornal invalido")
+    @Length(min = 1, max = 38, message = "Jornal invalido")
     private Integer codigo_site;
     @NotNull(message = "O campo 'data' não pode ser vazio")
     private LocalDate data;
