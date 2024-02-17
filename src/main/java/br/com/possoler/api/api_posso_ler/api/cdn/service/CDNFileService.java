20c2b5554cad4a21fc02629a593cc9ee01f5fcad
thomazf <thomaz.fneves@gmail.com>
2024-02-17 03:20:53 -0300

    criação de bean de configuração, exclusão de arquivo de constantes

--- src/main/java/br/com/possoler/api/api_posso_ler/api/cdn/service/CDNFileService.java
+++ src/main/java/br/com/possoler/api/api_posso_ler/api/cdn/service/CDNFileService.java
@@ -1,7 +1,6 @@
 package br.com.possoler.api.api_posso_ler.api.cdn.service;
 
-import br.com.possoler.api.api_posso_ler.api.cdn.constants.CDNEnum;
-import br.com.possoler.api.api_posso_ler.api.utils.PathConstants;
+import br.com.possoler.api.api_posso_ler.api.cdn.config.CDNConfigs;
 import exceptions.NotFoundException;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,120 +10,37 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class CDNFileService {
 
     @Autowired
     private HttpServletResponse response;
-    //private final String CDN_PATH = System.getProperty("user.dir") + PathConstants.FOLDER_POSSOLER_INTEGRATOR + "/CDN/";
-    private final String CDN_PATH = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\CDN\\";
+    private final String CDN_PATH;
+    private Map<String, String> files = new HashMap<>();
+    private final CDNConfigs cdnConfigs;
+
+    public CDNFileService(CDNConfigs cdnConfigs) {
+        this.cdnConfigs = cdnConfigs;
+        CDN_PATH = this.cdnConfigs.setCDNPath();
+        files = this.cdnConfigs.createCDNFilesMap();
+    }
 
     public void readCdnFile(String fileParamName) throws IOException, NotFoundException {
-        String filename = getFileName(fileParamName);
-        File file = new File(CDN_PATH + filename);
+        String filepath = getFilepath(fileParamName);
+        File file = new File(CDN_PATH + filepath);
         FileInputStream inputStream = new FileInputStream(file);
         IOUtils.copy(inputStream, response.getOutputStream());
         response.flushBuffer();
     }
 
-    private String getFileName(String file) {
-        if(file.equalsIgnoreCase(CDNEnum.BLOCK_CORE_PAYWALL.getFileParamName())) {
-            return CDNEnum.BLOCK_CORE_PAYWALL.getFilename();
+    private String getFilepath(String fileAlias) {
+        var filepath = files.get(fileAlias);
+        if(filepath == null) {
+            throw new NotFoundException("Arquivo CDN não encontrado");
         }
-        if(file.equalsIgnoreCase(CDNEnum.SNACKBAR_JS.getFileParamName())) {
-            return CDNEnum.SNACKBAR_JS.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.SNACKBAR_CSS.getFileParamName())) {
-            return CDNEnum.SNACKBAR_CSS.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.POSSOLER_LOGO.getFileParamName())) {
-            return CDNEnum.POSSOLER_LOGO.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.AXIOS_LOGO.getFileParamName())) {
-            return CDNEnum.AXIOS_LOGO.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.MAVEN_LOGO.getFileParamName())) {
-            return CDNEnum.MAVEN_LOGO.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.LOMBOK_LOGO.getFileParamName())) {
-            return CDNEnum.LOMBOK_LOGO.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.SPRING_LOGO.getFileParamName())) {
-            return CDNEnum.SPRING_LOGO.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.RESPONDE_AI_LOGO.getFileParamName())) {
-            return CDNEnum.RESPONDE_AI_LOGO.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.GLIDE_JS.getFileParamName())) {
-            return CDNEnum.GLIDE_JS.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.GLIDE_CORE_CSS.getFileParamName())) {
-            return CDNEnum.GLIDE_CORE_CSS.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.GLIDE_THEME_CSS.getFileParamName())) {
-            return CDNEnum.GLIDE_THEME_CSS.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_APP_RESPAI.getFileParamName())) {
-            return CDNEnum.EXTENSION_APP_RESPAI.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_RESPAI.getFileParamName())) {
-            return CDNEnum.EXTENSION_RESPAI.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_VLR_ECON.getFileParamName())) {
-            return CDNEnum.EXTENSION_VLR_ECON.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_EXAME.getFileParamName())) {
-            return CDNEnum.EXTENSION_EXAME.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_JOTA.getFileParamName())) {
-            return CDNEnum.EXTENSION_JOTA.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_NYTIMES.getFileParamName())) {
-            return CDNEnum.EXTENSION_NYTIMES.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_ELPAIS.getFileParamName())) {
-            return CDNEnum.EXTENSION_ELPAIS.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_GAUCHAZH.getFileParamName())) {
-            return CDNEnum.EXTENSION_GAUCHAZH.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_OPOPULAR.getFileParamName())) {
-            return CDNEnum.EXTENSION_OPOPULAR.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_DIARIOSM.getFileParamName())) {
-            return CDNEnum.EXTENSION_DIARIOSM.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_OTEMPOMG.getFileParamName())) {
-            return CDNEnum.EXTENSION_OTEMPOMG.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_JORNALDOCOMERCIO.getFileParamName())) {
-            return CDNEnum.EXTENSION_JORNALDOCOMERCIO.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_OPOVO.getFileParamName())) {
-            return CDNEnum.EXTENSION_OPOVO.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_GAZ.getFileParamName())) {
-            return CDNEnum.EXTENSION_GAZ.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_POSSOLER.getFileParamName())) {
-            return CDNEnum.EXTENSION_POSSOLER.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_ATT_VERSAO.getFileParamName())) {
-            return CDNEnum.EXTENSION_ATT_VERSAO.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_CHECK_MESSAGES.getFileParamName())) {
-            return CDNEnum.EXTENSION_CHECK_MESSAGES.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_COUNT_CONTENT.getFileParamName())) {
-            return CDNEnum.EXTENSION_COUNT_CONTENT.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_SAVE_SITE_ACCESS.getFileParamName())) {
-            return CDNEnum.EXTENSION_SAVE_SITE_ACCESS.getFilename();
-        }
-        if(file.equalsIgnoreCase(CDNEnum.EXTENSION_BLOCK_REQUEST.getFileParamName())) {
-            return CDNEnum.EXTENSION_BLOCK_REQUEST.getFilename();
-        }
-        throw new NotFoundException("Arquivo CDN não encontrado");
+        return filepath;
     }
 }
